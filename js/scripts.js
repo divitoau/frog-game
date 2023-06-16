1a02343e2d10713fbb9cda7e575ab9031fc81a2b
divitoau <divitoau@gmail.com>
2023-06-16 05:36:17 -0400

    added a bug for the frog to eat

--- js/scripts.js
+++ js/scripts.js
@@ -9,62 +9,91 @@ for (let i = 1; i < 33; i++) {
 let frog = document.querySelector('.frog');
 let startPad = document.querySelector('#lily-pad-1');
 startPad.appendChild(frog);
+let bug = document.createElement('img');
+bug.setAttribute('src', 'img/bug.png');
+bug.classList.add('bug');
 function getPadNumber() {
     let currentPad = frog.parentElement;
     let currentPadNumber = parseInt((currentPad.getAttribute('id')).substring(9));
     return currentPadNumber;
 }
 
-function jumpDown(number) {
-    if (number > 25) {
-        return number
-    } else {
-        return number + 8
-    }
-}
-
-function jumpUp(number) {
-    if (number < 8) {
-        return number
-    } else {
-        return number - 8
-    }
-}
-
-function jumpLeft(number) {
-    if (number === 1  ||
-        number === 9  ||
-        number === 17 ||
-        number === 25) {
-        return number
-    } else {
-        return number - 1
-        }
-}
-
-function jumpRight(number) {
-    if (number === 8  ||
-        number === 16 ||
-        number === 24 ||
-        number === 32) {
-        return number
-    } else {
-        return number + 1
-    }
-}
-
 function landOnNewPad(number) {
     let newPad = document.getElementById('lily-pad-' + number)
     newPad.appendChild(frog)
 }
+
+let frogJumps = (function () {
+    function down(number) {
+        if (number > 25) {
+            return number
+        } else {
+            return number + 8
+        }
+    }
+
+    function up(number) {
+        if (number < 8) {
+            return number
+        } else {
+            return number - 8
+        }
+    }
+
+    function left(number) {
+        if (number === 1 ||
+            number === 9 ||
+            number === 17 ||
+            number === 25) {
+            return number
+        } else {
+            return number - 1
+        }
+    }
+
+    function right(number) {
+        if (number === 8 ||
+            number === 16 ||
+            number === 24 ||
+            number === 32) {
+            return number
+        } else {
+            return number + 1
+        }
+    }
+
+    return { down, up, left, right }
+
+})();
+
 window.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowDown') {
-        landOnNewPad(jumpDown(getPadNumber()))
+        landOnNewPad(frogJumps.down(getPadNumber()))
     } else if (e.key === 'ArrowUp') {
-        landOnNewPad(jumpUp(getPadNumber()))
+        landOnNewPad(frogJumps.up(getPadNumber()))
     } else if (e.key === 'ArrowLeft') {
-        landOnNewPad(jumpLeft(getPadNumber()))
+        landOnNewPad(frogJumps.left(getPadNumber()))
     } else if (e.key === 'ArrowRight') {
-        landOnNewPad(jumpRight(getPadNumber()))
+        landOnNewPad(frogJumps.right(getPadNumber()))
     }
 });
+
+function getRandomInt() {
+    return Math.floor((Math.random() * 32) + 1)
+}
+
+function spawnBug(number) {
+    let bugSpawnPad = document.getElementById('lily-pad-' + number);
+    bugSpawnPad.appendChild(bug);
+}
+spawnBug(getRandomInt());
+
+function checkIfTogether() {
+    let bugPadCurrent = bug.closest('.lily-pad');
+    let frogPadCurrent = frog.closest('.lily-pad');
+    if (bugPadCurrent === frogPadCurrent) {
+        console.log('yum');
+        spawnBug(getRandomInt());
+    }
+}
+window.addEventListener('keydown', checkIfTogether)
