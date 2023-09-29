fe48f6a0f92160d988a6a408eeb42026b08e7451
divitoau <divitoau@gmail.com>
2023-09-28 21:02:36 -0400

    rewrote and reformatted a bunch, added a bomb, styled more reactively

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,145 @@
 for (let i = 1; i < 33; i++) {
-    let container = document.querySelector('.container');
-    let lilyPad = document.createElement('div');
-    lilyPad.classList.add('lily-pad');
-    lilyPad.setAttribute('id', 'lily-pad-' + i);
-    container.appendChild(lilyPad);
+  let lake = document.querySelector(".lake");
+  let lilyPad = document.createElement("div");
+  lilyPad.classList.add("lily-pad");
+  lilyPad.setAttribute("id", "lily-pad-" + i);
+  lake.appendChild(lilyPad);
 }
 
-let frog = document.querySelector('.frog');
-let startPad = document.querySelector('#lily-pad-1');
+let scoreText = document.querySelector(".score-text");
+let score = 0;
+scoreText.innerText = "Bugs: " + score;
+
+let frog = document.querySelector(".frog");
+let startPad = document.querySelector("#lily-pad-1");
 startPad.appendChild(frog);
-let bug = document.createElement('img');
-bug.setAttribute('src', 'img/bug.png');
-bug.classList.add('bug');
-function frogPadNumber() {
-    let currentPad = frog.parentElement;
-    let currentPadNumber = parseInt((currentPad.getAttribute('id')).substring(9));
-    return currentPadNumber;
-}
+
+let bug = document.createElement("img");
+bug.setAttribute("src", "img/bug.png");
+bug.classList.add("bug", "item");
+
+let bomb = document.createElement("img");
+bomb.setAttribute("src", "img/bomb.png");
+bomb.classList.add("bomb", "item");
+
+let currentFrogPadNumber = 1;
 
 function landOnNewPad(number) {
-    let newPad = document.getElementById('lily-pad-' + number)
-    newPad.appendChild(frog)
+  let newPad = document.getElementById("lily-pad-" + number);
+  newPad.appendChild(frog);
+  currentFrogPadNumber = number;
 }
 
 let frogJumps = (function () {
-    function down(number) {
-        if (number > 25) {
-            return number
-        } else {
-            return number + 8
-        }
+  function down(number) {
+    if (number > 25) {
+      return number;
+    } else {
+      return number + 8;
     }
+  }
 
-    function up(number) {
-        if (number < 8) {
-            return number
-        } else {
-            return number - 8
-        }
+  function up(number) {
+    if (number < 8) {
+      return number;
+    } else {
+      return number - 8;
     }
+  }
 
-    function left(number) {
-        if (number === 1 ||
-            number === 9 ||
-            number === 17 ||
-            number === 25) {
-            return number
-        } else {
-            return number - 1
-        }
+  function left(number) {
+    if ((number + 7) % 8 === 0) {
+      return number;
+    } else {
+      return number - 1;
     }
+  }
 
-    function right(number) {
-        if (number === 8 ||
-            number === 16 ||
-            number === 24 ||
-            number === 32) {
-            return number
-        } else {
-            return number + 1
-        }
+  function right(number) {
+    if (number % 8 === 0) {
+      return number;
+    } else {
+      return number + 1;
     }
+  }
 
-    return { down, up, left, right }
-
+  return { down, up, left, right };
 })();
 
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowDown') {
-        landOnNewPad(frogJumps.down(frogPadNumber()))
-    } else if (e.key === 'ArrowUp') {
-        landOnNewPad(frogJumps.up(frogPadNumber()))
-    } else if (e.key === 'ArrowLeft') {
-        landOnNewPad(frogJumps.left(frogPadNumber()))
-    } else if (e.key === 'ArrowRight') {
-        landOnNewPad(frogJumps.right(frogPadNumber()))
-    }
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowDown") {
+    landOnNewPad(frogJumps.down(currentFrogPadNumber));
+  } else if (e.key === "ArrowUp") {
+    landOnNewPad(frogJumps.up(currentFrogPadNumber));
+  } else if (e.key === "ArrowLeft") {
+    landOnNewPad(frogJumps.left(currentFrogPadNumber));
+  } else if (e.key === "ArrowRight") {
+    landOnNewPad(frogJumps.right(currentFrogPadNumber));
+  }
 });
 
+let currentBugPadNumber;
+
 function getRandomInt() {
-    return Math.floor((Math.random() * 32) + 1)
+  return Math.floor(Math.random() * 32 + 1);
 }
 
-function spawnBug(number) {
-    if (number === frogPadNumber() && number < 16) {
-        number = number + 17
-    } else if (number === frogPadNumber() && number > 15) {
-        number = number - 15
-    }
-    let bugSpawnPad = document.getElementById('lily-pad-' + number);
-    bugSpawnPad.appendChild(bug);
+function spawnBug() {
+  newBugPadNumber = getRandomInt();
+  while (newBugPadNumber === currentFrogPadNumber) {
+    newBugPadNumber = getRandomInt();
+  }
+  let bugSpawnPad = document.getElementById("lily-pad-" + newBugPadNumber);
+  bugSpawnPad.appendChild(bug);
+  currentBugPadNumber = newBugPadNumber;
 }
-spawnBug(getRandomInt());
 
-function checkIfTogether() {
-    let bugPadCurrent = bug.closest('.lily-pad');
-    let frogPadCurrent = frog.closest('.lily-pad');
-    if (bugPadCurrent === frogPadCurrent) {
-        console.log('yum');
-        spawnBug(getRandomInt());
-    }
+spawnBug();
+
+let cornerPadNumbers = [1, 8, 25, 32];
+let edgePadNumbers = [2, 3, 4, 5, 6, 7, 9, 16, 17, 24, 26, 27, 28, 29, 30, 31];
+let currentBombPadNumber;
+
+function spawnBomb() {
+  let newBombPadNumber = getRandomInt();
+  let occupiedPads = [currentBugPadNumber, currentFrogPadNumber];
+  while (occupiedPads.includes(newBombPadNumber)) {
+    newBombPadNumber = getRandomInt();
+  }
+  let bombSpawnPad = document.getElementById("lily-pad-" + newBombPadNumber);
+  bombSpawnPad.appendChild(bomb);
+  currentBombPadNumber = newBombPadNumber;
 }
-window.addEventListener('keydown', checkIfTogether);
+
+function dificultyUp() {
+  if (score > 4) {
+    spawnBomb();
+  }
+}
+
+function checkIfEatBug() {
+  if (currentBugPadNumber === currentFrogPadNumber) {
+    score++;
+    scoreText.innerText = "Bugs: " + score;
+    spawnBug();
+    dificultyUp();
+  }
+}
+
+function checkIfDead() {
+  if (currentBombPadNumber === currentFrogPadNumber) {
+    score = 0;
+    scoreText.innerText = "Bugs: " + score;
+    let bombPadElement = document.getElementById(
+      "lily-pad-" + currentBombPadNumber
+    );
+    bombPadElement.removeChild(bomb);
+    currentBombPadNumber = null;
+    startPad.appendChild(frog);
+    currentFrogPadNumber = 1;
+    spawnBug();
+  }
+}
+
+window.addEventListener("keydown", checkIfEatBug);
+window.addEventListener("keydown", checkIfDead);
